20cde3d2d4e84cd19ee9b47bc8ba4870bebdab03
Genesius-Julian-Muljadi <juliangenesiusmuljadi@gmail.com>
2025-03-03 13:57:46 +0700

    .

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -25,18 +25,6 @@ export default class Server {
         // optionsSuccessStatus: 200,
       })
     );
-    this.app.use((req, res, next) => {
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-      );
-      res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-      );
-      next();
-    });
 
     // this.app.options("*", (req, res) => {
     //   res.sendStatus(200);
