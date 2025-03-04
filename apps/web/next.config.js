ef2eb73db6edafb98f5452817ac305941352f83e
Genesius-Julian-Muljadi <juliangenesiusmuljadi@gmail.com>
2025-03-04 21:51:53 +0700

    -

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,20 +1,20 @@
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
-  style-src 'self' 'unsafe-inline';
-  img-src * blob: data:;
-  media-src *.s3.amazonaws.com;
-  connect-src *;
-  font-src 'self';
-  frame-src giscus.app
-`;
+// const ContentSecurityPolicy = `
+//   default-src 'self';
+//   script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
+//   style-src 'self' 'unsafe-inline';
+//   img-src * blob: data:;
+//   media-src *.s3.amazonaws.com;
+//   connect-src *;
+//   font-src 'self';
+//   frame-src giscus.app
+// `;
 
 const securityHeaders = [
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
-  {
-    key: "Content-Security-Policy",
-    value: ContentSecurityPolicy.replace(/\n/g, ""),
-  },
+  // {
+  //   key: "Content-Security-Policy",
+  //   value: ContentSecurityPolicy.replace(/\n/g, ""),
+  // },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
   {
     key: "Referrer-Policy",
