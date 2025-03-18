1dd7cb6f0f7bd17ec6bacb691d58e6b7b13c3761
Genesius-Julian-Muljadi <juliangenesiusmuljadi@gmail.com>
2025-03-18 14:26:38 +0700

    updated token expiration check

--- apps/web/src/verifytoken/verifytokenserver.ts
+++ apps/web/src/verifytoken/verifytokenserver.ts
@@ -1,44 +1,53 @@
-import { verify } from 'jsonwebtoken'
-import { jwtDecode } from 'jwt-decode'
-import { cookies } from 'next/headers'
-import COOKIE_EXPIRATION_MINUTES from '../../../cookieExpiration'
-import { AccessTokenUser } from '@/interfaces/accesstokens'
+import { verify } from "jsonwebtoken";
+import { jwtDecode } from "jwt-decode";
+import { cookies } from "next/headers";
+import { AccessTokenUser } from "@/interfaces/accesstokens";
+// import COOKIE_EXPIRATION_MINUTES from "../../../cookieExpiration";
 
 function checkExpired(token: AccessTokenUser): boolean {
-  const difference: number = new Date().valueOf() - new Date(token.iat * 1000).valueOf()
-  const expired: boolean = difference / (60 * 1000) > COOKIE_EXPIRATION_MINUTES
-  if (expired) console.log(difference / (60 * 1000) + ' minutes')
+  // const difference: number = new Date().valueOf() - new Date(token.iat * 1000).valueOf()
+  // const expired: boolean = difference / (60 * 1000) > COOKIE_EXPIRATION_MINUTES
+  // if (expired) console.log(difference / (60 * 1000) + ' minutes')
 
-  return expired
+  // return expired
+  if (token.exp) {
+    return new Date().valueOf() > new Date(token.exp).valueOf();
+  } else {
+    return false;
+  }
 }
 
 export default async function VerifyTokenServer(): Promise<AccessTokenUser | null> {
   try {
-    const cookieStore = await cookies()
-    const token = cookieStore.get('access_token_session')?.value
+    const cookieStore = await cookies();
+    const token = cookieStore.get("access_token_session")?.value;
 
-    let decodedToken: AccessTokenUser | null = null
+    let decodedToken: AccessTokenUser | null = null;
     if (token) {
-      decodedToken = jwtDecode(String(token))
-      const verified = verify(token, String(process.env.NEXT_PUBLIC_SECRET_KEY))
+      decodedToken = jwtDecode(String(token));
+      const verified = verify(
+        token,
+        String(process.env.NEXT_PUBLIC_SECRET_KEY)
+      );
       if (!verified) {
-        throw new Error('Unauthorized token!')
+        throw new Error("Unauthorized token!");
       }
     } else {
-      const newToken = cookieStore.get('access_token')?.value
+      const newToken = cookieStore.get("access_token")?.value;
       if (newToken) {
-        decodedToken = jwtDecode(String(newToken))
-        const verified2 = verify(newToken, String(process.env.NEXT_PUBLIC_SECRET_KEY))
+        decodedToken = jwtDecode(String(newToken));
+        const verified2 = verify(
+          newToken,
+          String(process.env.NEXT_PUBLIC_SECRET_KEY)
+        );
         if (!verified2) {
-          throw new Error('Unauthorized token!')
+          throw new Error("Unauthorized token!");
         }
       }
     }
 
-    return decodedToken && !checkExpired(decodedToken) ? decodedToken : null
+    return decodedToken && !checkExpired(decodedToken) ? decodedToken : null;
   } catch (err) {
-    // console.log('something went wrong in verifytokenserver')
-    // console.log('myerror: ' + err)
-    throw err
+    throw err;
   }
 }
