1304ca949d5a542f84395841d8031605cd997be8
Genesius-Julian-Muljadi <juliangenesiusmuljadi@gmail.com>
2025-04-02 13:57:38 +0700

    reverted data changes

--- apps/web/src/data/headerData.js
+++ apps/web/src/data/headerData.js
@@ -1,7 +1,7 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const headerData = {
   stickyNav: true,
-  headerLogo: "https://i.imgur.com/fLA4Tpg.jpeg",
+  headerLogo: "",
   headerLogoWidth: 500,
   headerLogoHeight: 500,
   headerTitle: "",
